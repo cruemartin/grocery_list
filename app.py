68fda5903c4a44092debe59b091b3668a4939c1e
cruemartin <crue.c.martin@wmich.edu>
2022-05-25 16:24:09 -0400

    Created basic templates and db tables

--- app.py
+++ app.py
@@ -1,10 +1,34 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///grocery_list.db'
+db = SQLAlchemy(app)
 
-@app.route('/')
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable = False)
+    price = db.Column(db.Float, nullable = False)
+    
+    def __repr__(self):
+        return '<Item %r'%self.id
+
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return 'Hello, World!'
+    if request.method =='POST':
+        name = request.form['name']
+        price = float(request.form['price'])
 
+        new_item = Item(name =name, price = price)
+
+        try:
+            db.session.add(new_item)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'Error with db commit.'
+    else:
+        items = Item.query.all()
+        return render_template('index.html', items = items)
 if __name__ == '__main__':
     app.run(debug=True)
